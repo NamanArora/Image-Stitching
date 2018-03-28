0806d4710ce02f7dbbc9c77c51b337ca6ab435db
DravitLochan <dlochangupta@gmail.com>
2018-03-28 23:52:13 +0530

    updated file name; load images

--- launcher.py
+++ launcher.py
@@ -1,14 +1,22 @@
+# add the images in left to right order
+# make sure no image is greater than 100kb
+
 import sys
 import cv2
 
+# initialize empty lists here
+
+imageFilesPath = []
+images = []
 left = []
 right = []
 flann = cv2.FlannBasedMatcher()
 
+
 def stitchLeft():
     img1 = left[0]
-    for i in range(1,len(left)):
-        #Feature extraction 
+    for i in range(1, len(left)):
+        # Feature extraction
         featureSet1 = getFeatures(img1)
         featureSet2 = getFeatures(left[i])
         desc1 = featureSet1[0]
@@ -16,14 +24,24 @@ def stitchLeft():
         kp1 = featureSet1[1]
         kp2 = featureSet2[1]
 
-        #Image Matching
-        matcher  = flann
+        # Image Matching
+        matcher = flann
 
 
-        #Finding homography
-
+        # Finding homography
 
 
 if __name__ == '__main__':
     args = sys.argv[1]
-    print "File loaded : " + args[1]
+    print "File loaded : " + args
+    pathToImagesFile = open(args, 'r')
+
+    for path in pathToImagesFile.readlines():
+        imageFilesPath.append(path.rstrip('\r\n'))
+
+    print imageFilesPath
+
+    for imageFilePath in imageFilesPath:
+        images.append(cv2.resize(cv2.imread(imageFilePath), (480, 320)))
+
+    print images
