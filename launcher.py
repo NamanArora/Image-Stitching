24dc0c2f4d0fb24b8734a7ee51ba1a53f061feeb
NamanArora <naman.arora11@gmail.com>
2018-03-28 23:52:10 +0530

    Use BF matcher

--- launcher.py
+++ launcher.py
@@ -1,9 +1,10 @@
 import sys
+from matplotlib import pyplot as plt
 import cv2
 
 left = []
 right = []
-flann = cv2.FlannBasedMatcher()
+BF = cv2.BFMatcher(cv2.NORM_L2, crossCheck = False)
 
 def stitchLeft():
     img1 = left[0]
@@ -17,7 +18,10 @@ def stitchLeft():
         kp2 = featureSet2[1]
 
         #Image Matching
-        matcher  = flann
+        matches  = BF.match(desc1,desc2)
+        matches = sorted(matches, key = lambda x:x.distance)
+        img3 = cv2.drawMatches(img1,kp1,left[i],kp2,matches[:10], flags=2)       
+        plt.imshow(img3),plt.show() 
 
 
         #Finding homography
